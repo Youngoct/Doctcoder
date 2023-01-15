744dfeeca7ffa04efe9cb6623aa2419e0d57627e
Youngoct <youngcoder42@gmail>
2023-01-15 21:23:24 +0100

    math

---
+++ C_programming/add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main-calculate the sum of the vale to the variable
+*assign
+*
+*Return: 0 as sucess
+*/
+int main(void)
+{
+
+	int k = 13670000;
+	int f = 12700000;
+	int s = k + f;
+
+	printf("total = %i\n",s);
+
+	return (0);
+}
