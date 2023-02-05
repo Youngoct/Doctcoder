ba361e43076aff82b77ad0405770f6e548e6f092
Doctware <yusclever9@gmail.com>
2023-02-05 16:15:02 +0100

    if_else.c

---
+++ C_programming/if_else.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - print the resuit
+ *
+ * Return:0 as success
+*/
+
+int main(void)
+{
+	float k = 5000.00;
+	float s = 1000.00;
+
+	if (k > s)
+	{
+		printf("yes\n");
+	}
+
+	else
+	{
+		printf("no\n");
+	}
+	return (0);
+}
