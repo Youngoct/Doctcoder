71df73793fc0c87c0cee7b543e2549aa7a671297
Youngoct <youngcoder42@gmail>
2023-02-18 12:36:51 +0100

    if_and_else.c

---
+++ C_programming/if_and_else.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the result
+ *
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int s = 10;
+	int k = 5;
+
+	if (s > k)
+
+	{
+		printf("'s' is the boss/oga\n");
+	}
+
+	else
+	{
+		printf("'k' is the boss/oga\n");
+	}
+	return (0);
+}
