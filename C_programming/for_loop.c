bdde74cb59f7a2ab36caa806c5e6781daf02e6d8
Doctware <yusclever9@gmail.com>
2023-02-19 15:23:24 +0100

    for_loop.c

---
+++ C_programming/for_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print my number
+ *
+ * Return:0 as succsse
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		printf("%i\n", a);
+	}
+	return (0);
+}
