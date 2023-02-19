aa5e76431c8682ce6e9a495d6d5a0cab5b402a9c
Doctware <yusclever9@gmail.com>
2023-02-19 15:24:03 +0100

    yloop.c

---
+++ C_programming/yloop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print my number
+ *
+ * Return: 0 as success
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 200)
+	{
+		printf("%i\n", a);
+		a++;
+	}
+	return (0);
+}
